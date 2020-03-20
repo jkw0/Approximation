205fd58d17d7846940573a527e4e107b8d246b4a
Justyna <justyna.walkowiak6969@gmail.com>
2020-03-20 19:45:21 +0100

    dla Marcinka

--- PointsGenerator.cpp
+++ PointsGenerator.cpp
@@ -10,7 +10,7 @@ PointsGenerator::PointsGenerator(PointsGenerator::Settings settings_)
 Points PointsGenerator::generatePoints(uint numberOfPoints)
 {
     Points points;
-    for (auto i = 0; i < numberOfPoints; i++)
+    for (auto i = 0; i < numberOfPoints; ++i)
         points.insert(generateOnePoint());
 
     return points;
@@ -38,9 +38,68 @@ Point PointsGenerator::generateOnePoint()
     double x = valueX;
     valueX += 0.5;
 
-    double y = generateRandomNumber(settings.minY, settings.maxY) * exp;
+    double y = generateRandomNumber(settings.minY, settings.maxY); //*exp
 
     std::cout << x << " " << y << std::endl;
 
     return Point(x,y);
+}
+
+
+AxisX generateAxisX(double min, double max, double step)
+{
+    AxisX axisX{};
+    for(auto x = min; x <= max; x+=step)
+    {
+        axisX.push_back(x);
+    }
+    return axisX;
+}
+
+AxisX PointsGenerator::generateAxisX(double min, uint32_t numOfPoints, double step)
+{
+    AxisX axisX{};
+    for(auto i = 0; i < numOfPoints; ++i)
+    {
+        axisX.push_back(min+i*step);
+    }
+
+    // worse function ;) wink wink
+    // for(auto x = min; x < min + numOfPoints*step; x+=step)
+    // {
+    //     axisX.push_back(x);
+    // }
+
+    return axisX;
+}
+
+AxisY PointsGenerator::generateAxisY(AxisX axisX, double coeffA, double coeffB, double coeffRandom)
+{
+    AxisY axisY{};
+    
+    for(auto x : axisX)
+    {
+        axisY.push_back(coeffA*exp(coeffB*x) + coeffRandom*generateRandomNumber(0, 5.0));
+    }
+    return axisY;
+}
+
+Points PointsGenerator::generateDataset(double /*min*/, double /*max*/, double /*step*/, double /*coeffA*/, double /*coeffB*/, double /*coeffRandom*/ )
+{
+    //!@TODO @Justynka please implement this function
+}
+
+Points PointsGenerator::generateDataset(double min, double step, uint32_t numOfPoints, double coeffA, double coeffB, double coeffRandom )
+{
+    AxisX axisX = generateAxisX(min, numOfPoints, step);
+    AxisY axisY = generateAxisY(axisX, coeffA, coeffB, coeffRandom);
+
+    Points points{};
+    for(auto i = 0; i < numOfPoints; ++i)
+    {
+        points.insert({axisX.at(i), axisY.at(i)});
+        std::cout << axisX.at(i) << " " << axisY.at(i) << std::endl;
+    }
+
+    return points;
 }
--- PointsGenerator.hpp
+++ PointsGenerator.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include <map>
+#include <vector>
 
 using Point = std::pair<double, double>;
 using Points = std::map<double, double>;
+using AxisX = std::vector<double>;
+using AxisY = std::vector<double>;
 
 class PointsGenerator
 {
@@ -15,8 +18,14 @@ class PointsGenerator
     double generateRandomNumber(double min, double max);
     Point generateOnePoint();
 
+    AxisX generateAxisX(double min, double max, double step);
+    AxisX generateAxisX(double min, uint32_t numOfPoints, double step);
+    AxisY generateAxisY(AxisX axisX, double coeffA, double coeffB, double coeffRandom);
+
 public:
     PointsGenerator(Settings settings_);
     Points generatePoints(uint numberOfPoints);
     void setNewSettings(Settings settings_);
+    Points generateDataset(double min, double max, double step, double coeffA = 2, double coeffB = -0.2, double coeffRandom = 0.1 );
+    Points generateDataset(double min, double step, uint32_t numOfPoints, double coeffA = 2, double coeffB = -0.2, double coeffRandom = 0.1 );
 };
