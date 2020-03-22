5f0d40f4061be2ad7f6ac1903fb156eeed0cb96f
Justyna <justyna.walkowiak6969@gmail.com>
2020-03-22 17:17:26 +0100

    begin spline implementation

--- Spline.cpp
+++ Spline.cpp
@@ -1,1 +1,16 @@
 #include "Spline.hpp"
+
+Spline::Spline(double minX, double maxX, double step)
+    : Approximation(minX, maxX, step)
+{
+}
+
+void Spline::calculate()
+{
+
+}
+
+void Spline::printCoefficients() const
+{
+
+}
--- Spline.hpp
+++ Spline.hpp
@@ -3,5 +3,8 @@
 
 class Spline : public Approximation
 {
-
+public:
+    Spline(double minX, double maxX, double step);
+    void calculate();
+    void printCoefficients() const;
 };
